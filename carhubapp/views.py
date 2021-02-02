058407e276ffac209d184c3e2a9425e07f5b7881
Kerrykogei24 <kerrykomar.gmail.com>
2021-02-02 23:39:16 +0300

    create a newsletter functin

--- carhubapp/views.py
+++ carhubapp/views.py
@@ -85,3 +85,15 @@ def unfollow(request, user_id):
 
     return redirect('single-car')
 
+
+def newsletter(request):
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
+    
+    recipient = NewsLetterRecipients(name = name, email = email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
+
+
