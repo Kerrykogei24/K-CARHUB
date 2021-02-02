227f94b4d8acf4a441e5e47cbfaef082637a7a1c
Kerrykogei24 <kerrykomar.gmail.com>
2021-02-02 23:44:46 +0300

    created views urls

--- carhubapp/urls.py
+++ carhubapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,6 +7,14 @@ from django.urls import path
 urlpatterns=[
     url(r'^$',views.index,name='home'),
     url(r'^logout/$',views.logout_request,name="logout"),
+    path('postImage/', views.post_image, name='postImage'),
+    path('photos', views.photos, name='photos'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/settings', views.edit_profile, name='edit'),
+    path('single_art/<art_id>', views.single_car, name='single-car'),
+    path('unfollow/<user_id>', views.unfollow, name='unfollow'),
+    path('follow/<user_id>', views.follow, name='follow'),
+    
 
 
 
